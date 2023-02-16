2d94657c113dc5af4e9cc7498aefd26d7e5bb8ea
Julianchos <julianchos.rivera@gmail.com>
2023-02-15 19:34:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
@@ -7,16 +6,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         App Pokedex
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Julian Rivera
+        </p>
       </header>
     </div>
   );
